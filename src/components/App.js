bb3c8b6687690f5fe842c5f033752622588197b5
AndresFMoya <andresfmoya@gmail.com>
2020-01-10 17:21:56 -0500

    feat: implement App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPannel from './ButtonPannel';
+
+function App() {
+  return (
+    <div>
+      <Display />
+      <ButtonPannel />
+    </div>
+  );
+}
+
+export default App;
